15c3a26e566f32e0b15aa4ff388d0845ac3a6c23
christophwottawa <christoph.wottawa@me.com>
2019-07-09 12:32:07 +0200

    first page

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Codeforge',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Shape the World in Code</h1>
+    <p>Welcome to the Codeforge, where we learn to forge ideas into code.</p>
+    <p>Now let's go build something great together.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
