e3b23d32746478ceff2f72d94ad2a81f56c5f6f9
christophwottawa <christoph.wottawa@me.com>
2019-07-12 22:33:55 +0200

    added some styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,15 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Shape the World in Code</h1>
-    <p>Welcome to the Codeforge, where we learn to forge ideas into code.</p>
-    <p>Now let's go build something great together.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="hero">
+      <div className="heroGroup">
+        <h1>Shape the World in Code</h1>
+        <p>Welcome to the Codeforge, where we learn to forge ideas into code.</p>
+        <p>Now let's go build something great together.</p>
+        <Link to="/video">Watch the video</Link>
+      </div>
+    </div>
+
   </div>
 )
 
